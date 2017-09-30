61c9a1a6c07dad4afc200f834cc67475faf133ad
wupeng <494572656@qq.com>
2017-09-30 09:49:32 +0800

    修改结构

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'sd-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   slides: Array<any> = [];
-  slogan: Array<String> = [];
+  slogan: Array<String> = [
+    '免费简约的问卷系统',
+    '简单 好用 在线调查网站',
+    '多方式创建编辑问卷'
+  ];
 
-  ngOnInit() {
+  constructor() {
+    for (let i = 0; i < 3; i++) {
+      this.addSlide(i);
+    }
+  }
+
+  addSlide(idx: number) {
+    this.slides.push({
+      image: `../assets/img/home/banner_0${idx + 1}.jpg`,
+      text: this.slogan[idx]
+    });
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,15 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+import {CarouselModule} from 'ng2-bootstrap';
 import {HomeComponent} from './home.component';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
-  imports: [],
+  imports: [ CommonModule, CarouselModule, BrowserModule],
   exports: [HomeComponent],
   providers: []
 })
